cc277ca7c7d2470a8703aa6e9866a21570d3b5c2
Rafael Leonardo da Cruz <rafael.leonardo@creditas.com.br>
2020-03-29 22:55:00 -0300

    Repositories setup

---
+++ org-setup/repository.tf
@@ -0,0 +1,32 @@
+resource "github_repository" "front" {
+  name        = "front"
+  description = "repository for frontend code"
+
+  private = false
+
+}
+
+resource "github_repository" "back" {
+  name        = "back"
+  description = "repository for frontend code"
+
+  private = false
+
+}
+
+resource "github_repository" "secret" {
+  name        = "secret"
+  description = "repository for frontend code"
+
+  private = false
+
+}
+
+resource "github_repository" "project" {
+  name        = "project"
+  description = "repository for frontend code"
+
+  private = false
+
+}
+
